d727b2542691477951144087e9140b675d103fe1
KanawadeGayatri <100459993+KanawadeGayatri@users.noreply.github.com>
2023-10-17 23:25:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
-function dis(val) { 
-    document.getElementById("result").value += val 
-} 
+function submitForm() {
+   
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const dob = document.getElementById('dob').value;
+    const country = document.getElementById('country').value;
+    const gender = document.getElementById('male').checked ? 'Male' : (document.getElementById('female').checked ? 'Female' : '');
+    const profession = document.getElementById('profession').value;
+    const email = document.getElementById('email').value;
+    const mobile = document.getElementById('mobile').value;
 
-function myFunction(event) { 
-    if (event.key == '0' || event.key == '1' 
-        || event.key == '2' || event.key == '3' 
-        || event.key == '4' || event.key == '5' 
-        || event.key == '6' || event.key == '7' 
-        || event.key == '8' || event.key == '9' 
-        || event.key == '+' || event.key == '-' 
-        || event.key == '*' || event.key == '/') 
-        document.getElementById("result").value += event.key; 
-} 
+   
+    const submittedData = `
+        <p><strong>First Name:</strong> ${firstName}</p>
+        <p><strong>Last Name:</strong> ${lastName}</p>
+        <p><strong>Date of Birth:</strong> ${dob}</p>
+        <p><strong>Country:</strong> ${country}</p>
+        <p><strong>Gender:</strong> ${gender}</p>
+        <p><strong>Profession:</strong> ${profession}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Mobile Number:</strong> ${mobile}</p>
+    `;
 
-var cal = document.getElementById("calcu"); 
-cal.onkeyup = function (event) { 
-    if (event.keyCode === 13) { 
-        console.log("Enter"); 
-        let x = document.getElementById("result").value 
-        console.log(x); 
-        solve(); 
-    } 
-} 
+    document.getElementById('submittedData').innerHTML = submittedData;
 
-// Function that evaluates the digit and return result 
-function solve() { 
-    let x = document.getElementById("result").value 
-    let y = math.evaluate(x) 
-    document.getElementById("result").value = y 
-} 
+   
+    document.getElementById('popup').style.display = 'block';
 
-// Function that clear the display 
-function clr() { 
-    document.getElementById("result").value = "" 
-} 
+    
+    resetForm();
+}
+
+function resetForm() {
+    document.getElementById('surveyForm').reset();
+}
+
+function closePopup() {
+    
+    document.getElementById('popup').style.display = 'none';
+}
